c0427a2f532eb02a2ef12be4fa6ab9d4edc2e562
devs.hashtag <devs.hashtag@gmail.com>
2023-06-04 13:38:42 +0330

    double qoutes changed to single quotes

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,32 +1,32 @@
-import { categoryTemplate, cardTemplate, getJSON } from "/assets/js/modules/template.js";
+import { categoryTemplate, cardTemplate, getJSON } from '/assets/js/modules/template.js';
 
-const projectsCategories = document.querySelector(".projects__categories");
-const cardCategories = document.querySelector(".category__cards");
+const projectsCategories = document.querySelector('.projects__categories');
+const cardCategories = document.querySelector('.category__cards');
 
 function setModalEvent() {
-  projectsCategories.addEventListener("click", (e) => {
+  projectsCategories.addEventListener('click', (e) => {
     try {
       const elm = e.target;
-      const categoryCard = elm.closest(".category__card");
-      const modalClose = categoryCard.querySelector(".modal__header button i");
+      const categoryCard = elm.closest('.category__card');
+      const modalClose = categoryCard.querySelector('.modal__header button i');
 
       const modalToggles = [
         // modal - open
-        elm.classList.contains("card__caption"),
-        elm.parentElement.classList.contains("card__image"),
+        elm.classList.contains('card__caption'),
+        elm.parentElement.classList.contains('card__image'),
         // modal - close
-        elm.classList.contains("card__modal"),
+        elm.classList.contains('card__modal'),
         elm === modalClose,
       ];
 
       // toggle modal
       if (modalToggles.includes(true)) {
-        if (categoryCard.classList.contains("modal--active")) {
-          categoryCard.classList.toggle("modal--active");
-          document.body.classList.toggle("no-scroll");
+        if (categoryCard.classList.contains('modal--active')) {
+          categoryCard.classList.toggle('modal--active');
+          document.body.classList.toggle('no-scroll');
         } else {
-          document.body.classList.toggle("no-scroll");
-          categoryCard.classList.toggle("modal--active");
+          document.body.classList.toggle('no-scroll');
+          categoryCard.classList.toggle('modal--active');
         }
       }
     } catch (err) {
@@ -35,13 +35,12 @@ function setModalEvent() {
   });
 }
 
-const projectCardsAPI = "/assets/js/data/projects.json";
-
+const projectCardsAPI = '/assets/js/data/projects.json';
 const projectCards = await getJSON(projectCardsAPI);
 
 for (const card of projectCards) {
-  const category = card["category"];
-  const projects = card["projects"];
+  const category = card['category'];
+  const projects = card['projects'];
 
   let teamwork = [];
   let solo = [];
@@ -56,13 +55,13 @@ for (const card of projectCards) {
 
   // category teamwork
   const categoryTeamWorkHTML = categoryTemplate(category, true);
-  const teamworkCardsHTML = teamwork.reduce((html, current) => html + current, "");
-  cardCategories.insertAdjacentHTML("beforeend", categoryTeamWorkHTML + teamworkCardsHTML);
+  const teamworkCardsHTML = teamwork.reduce((html, current) => html + current, '');
+  cardCategories.insertAdjacentHTML('beforeend', categoryTeamWorkHTML + teamworkCardsHTML);
 
   // category solo
   const categorySoloHTML = categoryTemplate(category, false);
-  const soloCardsHTML = solo.reduce((html, current) => html + current, "");
-  cardCategories.insertAdjacentHTML("beforeend", categorySoloHTML + soloCardsHTML);
+  const soloCardsHTML = solo.reduce((html, current) => html + current, '');
+  cardCategories.insertAdjacentHTML('beforeend', categorySoloHTML + soloCardsHTML);
 }
 
 // set modal event
