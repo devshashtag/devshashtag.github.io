f63d39070213c0c1e51f084ea12a755e1f5b2682
devs.hashtag <devs.hashtag@gmail.com>
2024-08-20 13:30:50 +0330

    home sidebar-projects removed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,8 @@
-import {
-  homeSkillTemplate,
-  sidebarCategoryTemplate,
-  sidebarProjectTemplate,
-  getJSON,
-} from '/assets/js/modules/template.js';
+import { homeSkillTemplate, getJSON } from '/assets/js/modules/template.js';
+// , sidebarCategoryTemplate, sidebarProjectTemplate
 
 const infoList = document.getElementById('skill');
-const projectsList = document.getElementById('projects');
+// const projectsList = document.getElementById('projects');
 
 async function displaySkills() {
   const skillsAPI = '/assets/js/data/skills.json';
@@ -19,29 +15,37 @@ async function displaySkills() {
 
     for (const item of data['items']) {
       skills += homeSkillTemplate(item, delay);
-      delay += 0.1;
+      delay += 0.5;
     }
 
     infoList.insertAdjacentHTML('beforeend', header + skills);
   }
 }
 
-async function displayProjects() {
-  const projectsAPI = '/assets/js/data/projects.json';
-  const jsonProjects = await getJSON(projectsAPI);
+// <!-- projects -->
+// <section class="sidebar__projects">
+// <h2 class="projects__header">projects</h2>
+// <nav class="projects__list">
+// <ul id="projects"></ul>
+// </nav>
+// </section>
+//
+// async function displayProjects() {
+//   const projectsAPI = '/assets/js/data/projects.json';
+//   const jsonProjects = await getJSON(projectsAPI);
 
-  for (const data of jsonProjects) {
-    const categoryTemplate = sidebarCategoryTemplate(data['category']);
-    let projectsTemplate = '';
+//   for (const data of jsonProjects) {
+//     const categoryTemplate = sidebarCategoryTemplate(data['category']);
+//     let projectsTemplate = '';
 
-    for (const project of data['projects']) {
-      projectsTemplate += sidebarProjectTemplate(project);
-    }
+//     for (const project of data['projects']) {
+//       projectsTemplate += sidebarProjectTemplate(project);
+//     }
 
-    if (!projectsTemplate) continue;
-    projectsList.insertAdjacentHTML('beforeend', categoryTemplate + projectsTemplate);
-  }
-}
+//     if (!projectsTemplate) continue;
+//     projectsList.insertAdjacentHTML('beforeend', categoryTemplate + projectsTemplate);
+//   }
+// }
 
 displaySkills();
-displayProjects();
+// displayProjects();
